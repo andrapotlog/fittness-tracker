62d4380b3e92681ff8e92afa3f1601e9d8505b1e
Andra <andraapotlog@gmail.com>
2022-01-12 12:04:37 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fitness-tracker';
+
+  /* @ViewChild('sidenav')
+  onToggle() {
+    
+  } */
+}
