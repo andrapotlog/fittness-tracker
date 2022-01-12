5295ce43de8f7b3330a5fcbb9d6e4542941a0b8e
Andra Isabela Potlog <c00024801@rbro.rbg.cc>
2022-01-12 18:03:39 +0200

    end of module 4

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+
+import {StopTrainingComponent} from "./stop-training.component";
 
 @Component({
-  selector: 'app-current-training',
-  templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.scss']
+	selector: 'app-current-training',
+	templateUrl: './current-training.component.html',
+	styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+	progress = 0;
+	timer: number;
 
-  constructor() { }
+	@Output() trainingExit = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+	constructor(private dialog: MatDialog) { }
+
+	ngOnInit(): void {
+    this.startOrResumeTimer()
+	}
+
+	startOrResumeTimer() {
+		this.timer = setInterval(() => {
+			this.progress += 5;
+			if(this.progress >= 100) {
+				clearInterval(this.timer);
+			}
+		}, 1000);
+	}
+
+	onStop(){
+		clearInterval(this.timer);
+		const dialogRef = this.dialog.open(StopTrainingComponent,{data:{
+				progress: this.progress
+			}});
+
+		dialogRef.afterClosed().subscribe(result => {
+			if(result) {
+				this.trainingExit.emit( )
+			} else {
+        this.startOrResumeTimer()
+      }
+		})
+	}
 
 }
